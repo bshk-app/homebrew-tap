5b26df2fea963d71c5c01b1d5a07b7262bf37be4
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-06-25 21:13:15 +0300

    zamokctl 1.2.3

--- Formula/zamokctl.rb
+++ Formula/zamokctl.rb
@@ -1,9 +1,9 @@
 class Zamokctl < Formula
   desc "Drive the Zamok macOS release pipeline from the terminal"
   homepage "https://zamok01.bshk.app"
-  url "https://github.com/bshk-app/homebrew-tap/releases/download/zamokctl-1.2.2/zamokctl-1.2.2-macos-arm64.tar.gz"
-  version "1.2.2"
-  sha256 "207bacae19261f5dc03f4489d3aa2bb83c47ab1655301c67ca324703e076d22b"
+  url "https://github.com/bshk-app/homebrew-tap/releases/download/zamokctl-1.2.3/zamokctl-1.2.3-macos-arm64.tar.gz"
+  version "1.2.3"
+  sha256 "f3a886c5f8f2620d2413ae0f40990b6b8fcda4272d4b3c3287dc32b629a2a06f"
   license :cannot_represent
 
   depends_on arch: :arm64
