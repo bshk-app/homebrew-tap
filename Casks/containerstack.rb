09b01ea80d223460191e3862ce1edf4ef8eac6ce
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-08-20 23:42:59 +0300

    cask containerstack 0.1.0

--- Casks/containerstack.rb
+++ Casks/containerstack.rb
@@ -1,8 +1,8 @@
 cask "containerstack" do
   version "0.1.0"
-  sha256 "4ba5ad70386df09959292270013e0f4f91b0e33563f3faf5aed264922e356605"
+  sha256 "be0ed1d04297ca0b75fa5cd7723731e77b8fb23e074be07849e749cb91a48ceb"
 
-  url "https://dl.bshk.app/products/containerstack/releases/0.1.0/389d0bcb-ad72-4a1d-93fa-79c583578914/ContainerStack-0.1.0.dmg"
+  url "https://dl.bshk.app/products/containerstack/releases/0.1.0/9a343b6f-8153-4244-944b-7f0d2673c5ef/ContainerStack-0.1.0.dmg"
   name "ContainerStack"
   desc "Native macOS container stack on Apple Container with a Docker-compatible socket"
   homepage "https://containerstack.bshk.app"
